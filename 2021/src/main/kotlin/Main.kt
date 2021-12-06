ba7fa43975fbb8fcb4d2b91ddb30e7a2e3732748
Maikulole <TimHiller@live.de>
2021-12-06 15:45:35 +0100

    solving day05

--- 2021/src/main/kotlin/Main.kt
+++ 2021/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import day03.Day03
 import day04.Day04
+import day05.Day05
 
 
 fun readInput(fileName: String): List<String>
@@ -23,10 +24,16 @@ fun main(args: Array<String>) {
      println("Day 03 Part 01: $day03part1")
      val day03part2 = day03.SolvePartTwo();
      println("Day 03 Part 02: $day03part2")
-     */
+
      var day04 = Day04(readInput("day04_input.txt"))
      val day04part1 = day04.SolvePartOne()
      println("Day 04 Part 01: $day04part1")
      val day04part2 = day04.SolvePartTwo()
      println("Day 04 Part 02: $day04part2")
+     */
+     var day05 = Day05(readInput("day05_input.txt"))
+     val day05part1 = day05.SolvePartOne()
+     println("Day 05 Part 01: $day05part1")
+     val day05part2 = day05.SolvePartTwo()
+     println("Day 05 Part 02: $day05part2")
 }
