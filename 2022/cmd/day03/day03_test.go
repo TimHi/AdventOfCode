7be3ab7bd6b3f786e5f41be9ed0846435cbb561d
TimHi <TimHiller@live.de>
2022-12-06 01:16:03 +0100

    adding day03 unit tests

---
+++ 2022/cmd/day03/day03_test.go
@@ -0,0 +1,34 @@
+package day03_test
+
+import (
+	"os"
+	"path"
+	"runtime"
+	"testing"
+
+	"github.com/TimHi/AdventOfCode/m/v2/cmd/day03"
+	"github.com/TimHi/AdventOfCode/m/v2/pkg/fileutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	// The ".." may change depending on you folder structure
+	dir := path.Join(path.Dir(filename), "../..")
+	err := os.Chdir(dir)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSolvePartOne(t *testing.T) {
+	input := fileutil.GetPuzzleInput(true, 3)
+	result := day03.SolvePartOne(input)
+	assert.Equal(t, 157, result, "Result was not matching")
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	input := fileutil.GetPuzzleInput(true, 3)
+	result := day03.SolvePartTwo(input)
+	assert.Equal(t, 70, result, "Result was not matching")
+}
