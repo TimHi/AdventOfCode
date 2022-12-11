5543b2e764f83b5a28bda03b94606e50e4694b58
TimHi <timhiller@live.de>
2022-12-11 19:10:09 +0100

    adding test

--- 2022/cmd/day11/day11_test.go
+++ 2022/cmd/day11/day11_test.go
@@ -1,6 +1,7 @@
 package day11_test
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"runtime"
@@ -31,4 +32,28 @@ func TestSolvePartTwo(t *testing.T) {
 	input := fileutil.GetStringInputs(true, 11)
 	result := day11.SolvePartTwo(input)
 	assert.Equal(t, 2713310158, result, "Result was not matching")
+}
+
+var WorryLevelTestData = []struct {
+	op1     string
+	op2     string
+	operand string
+	level   int
+	out     int
+}{
+	{"old", "old", "+", 5, 10},
+	{"old", "old", "*", 5, 25},
+	{"1", "old", "+", 5, 6},
+	{"old", "1", "+", 5, 6},
+	{"old", "2", "*", 5, 10},
+	{"2", "2", "+", 5, 4},
+	{"2", "8", "*", 5, 16},
+}
+
+func TestCalculateWorryLevel(t *testing.T) {
+	for _, testData := range WorryLevelTestData {
+		r := day11.CalculateWorryLevel(testData.op1, testData.op2, testData.operand, testData.level)
+		m := fmt.Sprintf("CalculateWorryLevel: %s %s %s Expected %d", testData.op1, testData.operand, testData.op2, testData.out)
+		assert.Equal(t, testData.out, r, m)
+	}
 }
---
+++ 2022/cmd/day11/export_test.go
@@ -0,0 +1,3 @@
+package day11
+
+var CalculateWorryLevel = calculateWorryLevel
