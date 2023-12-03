7d6a2de0339c47733d794cb32c50341fe1b1c021
TimHi <timhiller@live.de>
2023-12-03 14:05:09 +0100

    WIP

--- Typescript/2023/src/days/day03/day03.ts
+++ Typescript/2023/src/days/day03/day03.ts
@@ -1,22 +1,15 @@
 import * as fs from "fs";
 
-const isSample = true;
+const isSample = false;
 
 interface Point {
   X: number;
   Y: number;
 }
 
-interface Part {
-  Start: Point;
-  End: Point;
-  Value: string;
-}
-
 const ENGINE_SYMBOLS = ["*", "#", "$", "+", "&", "-", "/", "@", "=", "%"];
 
 const SYMBOL_MAP = new Map<Point, string>();
-const NUMBER_MAP = new Map<Point, Part>();
 
 export function SolvePartOne(): number {
   const fileName = isSample
@@ -26,7 +19,7 @@ export function SolvePartOne(): number {
   const engine = fs.readFileSync(process.cwd() + fileName, "utf8").split("\n");
   parseField(engine);
 
-  return checkSymbols();
+  return 0;
 }
 
 export function SolvePartTwo(): number {
@@ -34,108 +27,90 @@ export function SolvePartTwo(): number {
   return 0;
 }
 
-function parseField(lines: string[]) {
+function parseField(lines: string[]): number {
+  let sum = 0;
   const numberRegex = new RegExp("[0-9]+", "g");
   lines.forEach((line, yIndex) => {
     line.split("").forEach((char, xIndex) => {
       if (ENGINE_SYMBOLS.includes(char))
         SYMBOL_MAP.set({ X: xIndex, Y: yIndex }, char);
     });
-
-    const numberMatch = line.match(numberRegex);
-    let tempLine = line;
-    let offSet = 0;
-    if (numberMatch) {
-      numberMatch.forEach((foundNumber) => {
-        const startPoint = tempLine.indexOf(foundNumber);
-        if (checkNumberMapByProperty({ X: startPoint + offSet, Y: yIndex }))
-          console.error("Duplicate numbers in map, this approach wont work");
-        const num: Part = {
-          Start: { X: startPoint + offSet, Y: yIndex },
-          End: { X: startPoint + foundNumber.length - 1 + offSet, Y: yIndex },
-          Value: foundNumber
-        };
-        NUMBER_MAP.set({ X: startPoint + offSet, Y: yIndex }, num);
-        tempLine = tempLine.substring(
-          startPoint + foundNumber.length,
-          tempLine.length - 1
-        );
-        offSet += startPoint + foundNumber.length;
-      });
-    }
   });
-}
+  lines.forEach((line, yIndex) => {
+    const numberMatch = line.match(numberRegex);
 
-function checkSymbols(): number {
-  let sum = 0;
-  NUMBER_MAP.forEach((num) => {
-    for (let i = num.Start.X; i <= num.End.X; i++) {
-      if (checkMapByProperty({ X: i + 1, Y: num.Start.Y })) {
-        console.log("Valid Number: " + num.Value);
-        sum += Number(num.Value);
-        break;
-      }
-      if (checkMapByProperty({ X: i + 1, Y: num.Start.Y + 1 })) {
-        console.log("Valid Number: " + num.Value);
-        sum += Number(num.Value);
-        break;
-      }
-      if (checkMapByProperty({ X: i + 1, Y: num.Start.Y - 1 })) {
-        console.log("Valid Number: " + num.Value);
-        sum += Number(num.Value);
-        break;
-      }
-      if (checkMapByProperty({ X: i, Y: num.Start.Y - 1 })) {
-        console.log("Valid Number: " + num.Value);
-        sum += Number(num.Value);
-        break;
-      }
-      if (checkMapByProperty({ X: i, Y: num.Start.Y + 1 })) {
-        console.log("Valid Number: " + num.Value);
-        sum += Number(num.Value);
-        break;
-      }
-      if (checkMapByProperty({ X: i - 1, Y: num.Start.Y })) {
-        console.log("Valid Number: " + num.Value);
-        sum += Number(num.Value);
-        break;
-      }
-      if (checkMapByProperty({ X: i - 1, Y: num.Start.Y + 1 })) {
-        console.log("Valid Number: " + num.Value);
-        sum += Number(num.Value);
-        break;
-      }
-      if (checkMapByProperty({ X: i - 1, Y: num.Start.Y - 1 })) {
-        console.log("Valid Number: " + num.Value);
-        sum += Number(num.Value);
-        break;
-      }
+    if (numberMatch) {
+      numberMatch.forEach((foundNumber) => {});
     }
   });
   return sum;
 }
 
+function checkSymbols(start: number, end: number, y: number): boolean {
+  for (let i = start; i <= end; i++) {
+    if (checkMapByProperty({ X: i + 1, Y: y })) {
+      return true;
+    }
+    if (checkMapByProperty({ X: i + 1, Y: y + 1 })) {
+      return true;
+    }
+    if (checkMapByProperty({ X: i + 1, Y: y - 1 })) {
+      return true;
+    }
+    if (checkMapByProperty({ X: i, Y: y - 1 })) {
+      return true;
+    }
+    if (checkMapByProperty({ X: i, Y: y + 1 })) {
+      return true;
+    }
+    if (checkMapByProperty({ X: i - 1, Y: y })) {
+      return true;
+    }
+    if (checkMapByProperty({ X: i - 1, Y: y + 1 })) {
+      return true;
+    }
+    if (checkMapByProperty({ X: i - 1, Y: y - 1 })) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 function checkMapByProperty(targetKey: Point): boolean {
   let hasKey = false;
-  SYMBOL_MAP.forEach((_, key) => {
+  SYMBOL_MAP.forEach((v, key) => {
     if (key.X === targetKey.X && key.Y === targetKey.Y) {
-      hasKey = true;
-      return;
+      if (ENGINE_SYMBOLS.includes(v)) {
+        hasKey = true;
+        return;
+      }
     }
   });
   return hasKey;
 }
 
-function checkNumberMapByProperty(targetKey: Point): boolean {
-  let hasKey = false;
-  NUMBER_MAP.forEach((_, key) => {
-    if (key.X === targetKey.X && key.Y === targetKey.Y) {
-      hasKey = true;
-      return;
-    }
-  });
-  return hasKey;
-}
+// function checkMapByProperty(targetKey: Point): boolean {
+//   let hasKey = false;
+//   SYMBOL_MAP.forEach((_, key) => {
+//     if (key.X === targetKey.X && key.Y === targetKey.Y) {
+//       hasKey = true;
+//       return;
+//     }
+//   });
+//   return hasKey;
+// }
+
+// function checkNumberMapByProperty(targetKey: Point): boolean {
+//   let hasKey = false;
+//   NUMBER_MAP.forEach((_, key) => {
+//     if (key.X === targetKey.X && key.Y === targetKey.Y) {
+//       hasKey = true;
+//       return;
+//     }
+//   });
+//   return hasKey;
+// }
 
 //543466 too low
 //541173 too low
