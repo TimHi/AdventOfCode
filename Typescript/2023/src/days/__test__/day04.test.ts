d162083becc6566f404a6d2f05ece4c372eee7df
TimHi <timhiller@live.de>
2023-12-04 16:46:03 +0100

    Day 04 Part 02

--- Typescript/2023/src/days/__test__/day04.test.ts
+++ Typescript/2023/src/days/__test__/day04.test.ts
@@ -1,15 +1,14 @@
-
 import { SolvePartOne, SolvePartTwo } from "../day04/day04";
 import { describe, expect, test } from "vitest";
 
 describe("Day 04 Part 01", () => {
   test("Expected result", () => {
-    expect(SolvePartOne()).toBe(-1);
+    expect(SolvePartOne()).toBe(13);
   });
 });
 
 describe("Day 04 Part 02", () => {
   test("Expected result", () => {
-    expect(SolvePartTwo()).toBe(-1);
+    expect(SolvePartTwo()).toBe(30);
   });
 });
--- Typescript/2023/src/days/day04/day04.ts
+++ Typescript/2023/src/days/day04/day04.ts
@@ -1,40 +1,74 @@
 import * as fs from "fs";
 import _ from "lodash";
 
-const isSample = false;
+const isSample = true;
+const fileName = isSample ? "/src/days/day04/sample.txt" : "/src/days/day04/full.txt";
+
+const numReg = new RegExp("[0-9]+", "g");
+
+interface Game {
+  id: number;
+  winningNumbers: number[];
+  ownNumbers: number[];
+  wonCards: number;
+}
+
+const scoreMap = new Map<number, number[]>();
+function parseGames(): Game[] {
+  return fs
+    .readFileSync(process.cwd() + fileName, "utf8")
+    .split("\n")
+    .map((line) => line.substring(line.indexOf(":"), line.length))
+    .map((line) => line.split("|"))
+    .map((nums, gameIndex) => {
+      const winningNumbers = nums[0].match(numReg)?.map((v) => Number(v)) ?? [];
+      const ownNumbers = nums[1].match(numReg)?.map((v) => Number(v)) ?? [];
+      const wonCards = _.intersection(winningNumbers, ownNumbers).length;
+
+      const game: Game = { id: gameIndex, winningNumbers: winningNumbers, ownNumbers: ownNumbers, wonCards: wonCards };
+      scoreMap.set(
+        gameIndex,
+        Array.from({ length: wonCards }, (_, index) => gameIndex + index + 1)
+      );
+      return game;
+    });
+}
 
 export function SolvePartOne(): number {
-  const fileName = isSample ? "/src/days/day04/sample.txt" : "/src/days/day04/full.txt";
-  const numReg = new RegExp("[0-9]+", "g");
-  let sum = 0;
-  const scratchboards = fs
+  return fs
     .readFileSync(process.cwd() + fileName, "utf8")
     .split("\n")
     .map((line) => line.substring(line.indexOf(":"), line.length))
     .map((line) => line.split("|"))
     .map((nums) => {
+      let points = 0;
       const winningNumbers = nums[0].match(numReg)?.map((v) => Number(v));
-      const matches = nums[1].match(numReg);
-      const ownNumbers = matches?.map((v) => Number(v));
-      return [winningNumbers, ownNumbers];
-    });
-  scratchboards.forEach((game) => {
-    const intersection = _.intersection(game[0], game[1]);
-    let points = 0;
-    intersection.forEach((_, index) => {
-      if (index === 0) {
-        points = 1;
-      } else {
-        points = points * 2;
-      }
-    });
-    sum += points;
-  });
-
-  return sum;
+      const ownNumbers = nums[1].match(numReg)?.map((v) => Number(v));
+      _.intersection(winningNumbers, ownNumbers).forEach((_, index) => {
+        if (index === 0) points = 1;
+        else points = points * 2;
+      });
+      return points;
+    })
+    .reduce((sum, current) => sum + current);
 }
 
 export function SolvePartTwo(): number {
-  console.log("TBD");
-  return 0;
+  const games = parseGames();
+  const copies: number[] = [];
+  games.forEach((game) => {
+    const toCheck: number[] = [game.id];
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const id = toCheck.pop();
+      if (id !== undefined) {
+        const score = scoreMap.get(id);
+        if (score !== undefined && score.length > 0) {
+          copies.push(...score);
+          toCheck.push(...score);
+        }
+      } else break;
+    }
+  });
+  return copies.length + games.length;
 }
