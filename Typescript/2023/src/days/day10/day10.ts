58dccb3e7917491dd8762c598afb10840b9ce909
TimHi <timhiller@live.de>
2023-12-11 01:39:10 +0100

    dfs

--- Typescript/2023/src/days/day10/day10.ts
+++ Typescript/2023/src/days/day10/day10.ts
@@ -1,10 +1,11 @@
 import * as fs from "fs";
 import { Point } from "../../util/coords";
-import { DirectedGraph, UndirectedGraph } from "data-structure-typed";
 
 interface PipePosition {
   currentCoordinate: Point;
   pipe: string;
+  directions: Point[];
+  isStart: boolean;
 }
 
 function parsePipes() {
@@ -17,257 +18,180 @@ function parsePipes() {
   return pipes;
 }
 
-export function SolvePartOne(): number {
-  const pipes = parsePipes();
-  const pipeMap: PipePosition[][] = [];
-  const startPosition: Point = { X: 0, Y: 0 };
-  const graph = new UndirectedGraph<string>();
-  const symbolMap = new Map<Point, string>();
-  const pipeSymbols = ["|", "-", "L", "J", "7", "F", ".", "S"];
-  const groundMap: Point[] = [];
-  pipes.forEach((y, yIndex) => {
-    y.forEach((x, xIndex) => {
-      const coord: Point = { X: xIndex, Y: yIndex };
-      if (x === ".") groundMap.push(coord);
-      symbolMap.set(coord, x);
-    });
-  });
-
-  pipes.forEach((y, yIndex) => {
-    const translatedRow: PipePosition[] = [];
-    y.forEach((x, xIndex) => {
-      const coord: Point = { X: xIndex, Y: yIndex };
-      translatedRow.push({ currentCoordinate: coord, pipe: x });
-      if (x === "S") {
-        startPosition.X = coord.X;
-        startPosition.Y = coord.Y;
-      }
-      if (!isGround(coord, groundMap)) graph.addVertex(getNodeKey(coord));
-    });
-    pipeMap.push(translatedRow);
-  });
-
-  pipes.forEach((y, yIndex) => {
-    y.forEach((x, xIndex) => {
-      const coord: Point = { X: xIndex, Y: yIndex };
-      if (!isGround(coord, groundMap)) {
-        console.log("Getting Edges for: " + coord.X + ":" + coord.Y);
-        const edges = getEdges(x, coord, y.length, pipes.length);
-        edges.forEach((e) => {
-          if (!isGround(e, groundMap)) {
-            if (hasConnection(coord, e, symbolMap)) {
-              console.log("Adding Edge from: " + coord.X + ":" + coord.Y + " to: " + e.X + ":" + e.Y);
-              graph.addEdge(getNodeKey(coord), getNodeKey(e));
-              // if (!graph.hasEdge(getNodeKey(coord), getNodeKey(e))) {
-              //   console.log("Adding Edge from: " + coord.X + ":" + coord.Y + " to: " + e.X + ":" + e.Y);
-              //   graph.addEdge(getNodeKey(coord), getNodeKey(e));
-              // } else {
-              //   console.log("AAl");
-              // }
-            }
-          }
-        });
-      }
-    });
-  });
-
-  const c = graph.tarjan(undefined, undefined, undefined, true);
-
-  const cy = graph.getCycles();
-  cy.forEach((c, k) => {
-    console.log(c);
-    console.log(k);
-  });
-  const bF = graph.bellmanFord(getNodeKey({ X: 1, Y: 1 }));
-  bF.distMap.forEach((v, k) => {
-    console.log(k.key + " away: " + v);
-  });
-  return 0;
-}
-
-export function SolvePartTwo(): number {
-  console.log("TBD");
-  return 0;
-}
-
-function isGround(coord: Point, ground: Point[]): boolean {
-  const found = ground.find((v) => v.X === coord.X && v.Y === coord.Y);
-  return found !== undefined;
-}
-
-function getEdges(symbol: string, coord: Point, maxX: number, maxY: number): Point[] {
-  const directions: Point[] = [];
-  if (coord.X === 3 && coord.Y === 3) {
-    console.log("Aal");
-  }
-  const north: Point = { X: coord.X, Y: coord.Y - 1 };
-  const south: Point = { X: coord.X, Y: coord.Y + 1 };
-  const east: Point = { X: coord.X + 1, Y: coord.Y };
-  const west: Point = { X: coord.X - 1, Y: coord.Y };
-  switch (symbol) {
-    case "|": // | is a vertical pipe connecting north and south.
-      if (isInMap(north, maxX, maxY)) directions.push(north);
-      if (isInMap(south, maxX, maxY)) directions.push(south);
-      break;
-    case "-": // - is a horizontal pipe connecting east and west.
-      if (isInMap(east, maxX, maxY)) directions.push(east);
-      if (isInMap(west, maxX, maxY)) directions.push(west);
-      break;
-    case "L": // L is a 90-degree bend connecting north and east.
-      if (isInMap(east, maxX, maxY)) directions.push(east);
-      if (isInMap(north, maxX, maxY)) directions.push(north);
-      break;
-    case "J": // J is a 90-degree bend connecting north and west.
-      if (isInMap(west, maxX, maxY)) directions.push(west);
-      if (isInMap(north, maxX, maxY)) directions.push(north);
-      break;
-    case "7": // 7 is a 90-degree bend connecting south and west.
-      if (isInMap(west, maxX, maxY)) directions.push(west);
-      if (isInMap(south, maxX, maxY)) directions.push(south);
-      break;
-    case "F": // F is a 90-degree bend connecting south and east.
-      if (isInMap(east, maxX, maxY)) directions.push(east);
-      if (isInMap(south, maxX, maxY)) directions.push(south);
-      break;
-  }
-  return directions;
-}
-
-function isInMap(coord: Point, maxX: number, maxY: number) {
-  if (0 <= coord.X && coord.X < maxX && 0 <= coord.Y && coord.Y < maxY) return true;
-  else return false;
-}
-
-function getNodeKey(point: Point) {
-  return point.X.toString() + ":" + point.Y.toString();
-}
-function hasConnection(coord: Point, edge: Point, symbolMap: Map<Point, string>): boolean {
-  let current = ".";
-  let next = ".";
-  symbolMap.forEach((v, k) => {
-    if (coord.X === k.X && coord.Y === k.Y) current = v;
-    if (edge.X === k.X && edge.Y === k.Y) next = v;
-  });
-  if (current === ".") throw new Error("Wtf currently on ground?");
-  if (next === ".") throw new Error("No next tile found, this should not happen");
-  const direction = getDirection(coord, edge);
-  switch (direction) {
-    case Direction.North:
-      return isNorthPossible(current, next);
-    case Direction.South:
-      return isSouthPossible(current, next);
-    case Direction.West:
-      return isWestPossible(current, next);
-    case Direction.East:
-      return isEastPossible(current, next);
-    default:
-      console.log("FUCK ");
-      return false;
-  }
-}
-
-enum Direction {
-  North = "NORTH",
-  South = "SOUTH",
-  West = "WEST",
-  East = "EAST"
-}
-function getDirection(current: Point, next: Point): Direction {
-  const xDiff = next.X - current.X;
-  const yDiff = next.Y - current.Y;
-
-  if (xDiff === 1 && yDiff === 0) {
-    return Direction.East;
-  } else if (xDiff === -1 && yDiff === 0) {
-    return Direction.West;
-  } else if (xDiff === 0 && yDiff === 1) {
-    return Direction.South;
-  } else if (xDiff === 0 && yDiff === -1) {
-    return Direction.North;
-  } else {
-    throw new Error("Source the same as destination, shouldnt happen");
-  }
-}
-
-function isNorthPossible(current: string, next: string): boolean {
-  if (current === "|") {
-    if (next === "|") return true;
-    if (next === "7") return true;
-    if (next === "F") return true;
-  }
-  if (current === "L") {
-    if (next === "|") return true;
-    if (next === "7") return true;
-    if (next === "F") return true;
-  }
-  if (current === "J") {
-    if (next === "|") return true;
-    if (next === "7") return true;
-    if (next === "F") return true;
-  }
-
-  return false;
-}
-
-function isSouthPossible(current: string, next: string): boolean {
-  if (current === "|") {
-    if (next === "J") return true;
-    if (next === "|") return true;
-    if (next === "L") return true;
-  }
-  if (current === "7") {
-    if (next === "|") return true;
-    if (next === "J") return true;
-  }
-  if (current === "F") {
-    if (next === "|") return true;
-    if (next === "J") return true;
-  }
-
-  return false;
-}
-
-function isWestPossible(current: string, next: string): boolean {
-  if (current === "-") {
-    if (next === "-") return true;
-    if (next === "F") return true;
-    if (next === "L") return true;
-  }
-  if (current === "J") {
-    if (next === "-") return true;
-    if (next === "F") return true;
-    if (next === "L") return true;
-  }
-  if (current === "7") {
-    if (next === "-") return true;
-    if (next === "F") return true;
-    if (next === "L") return true;
-  }
-
-  return false;
-}
-
 // | is a vertical pipe connecting north and south.
 // - is a horizontal pipe connecting east and west.
 // L is a 90-degree bend connecting north and east.
 // J is a 90-degree bend connecting north and west.
 // 7 is a 90-degree bend connecting south and west.
 // F is a 90-degree bend connecting south and east.
-function isEastPossible(current: string, next: string): boolean {
-  if (current === "-") {
-    if (next === "-") return true;
-    if (next === "7") return true;
-    if (next === "J") return true;
+
+export function SolvePartOne(): number {
+  const pipes = parsePipes();
+  const pipeMap: PipePosition[][] = [];
+  const groundList: Point[] = [];
+
+  const startPosition: Point = { X: 0, Y: 0 };
+
+  pipes.forEach((y, yIndex) => {
+    const parsedPipes: PipePosition[] = [];
+    y.forEach((x, xIndex) => {
+      const pos: Point = { X: xIndex, Y: yIndex };
+      if (x === ".") {
+        groundList.push(pos);
+      } else if (x === "S") {
+        startPosition.X = xIndex;
+        startPosition.Y = yIndex;
+      }
+      const directions: Point[] = getDirections(x);
+      parsedPipes.push({ currentCoordinate: pos, pipe: x, directions: directions, isStart: false });
+    });
+    pipeMap.push(parsedPipes);
+  });
+  //const possibleSymbols = ["|", "-", "L", "J", "7", "F"];
+  const possibleSymbols = ["L"];
+  possibleSymbols.forEach((possibleStartPipe) => {
+    const copy: PipePosition[][] = deepCopyArray(pipeMap);
+    const directions = getDirections(possibleStartPipe);
+    copy[startPosition.Y][startPosition.X] = {
+      currentCoordinate: startPosition,
+      pipe: possibleStartPipe,
+      directions: directions,
+      isStart: true
+    };
+    console.log(possibleStartPipe);
+    const steps = testStartPipes(startPosition, copy, groundList, copy[0].length, copy.length);
+    console.log(steps);
+  });
+
+  return 0;
+}
+
+function testStartPipes(startPosition: Point, pipeMap: PipePosition[][], groundList: Point[], maxX: number, maxY: number): number {
+  const visited: boolean[][] = Array.from({ length: pipeMap.length }, () => Array(pipeMap[0].length).fill(false));
+
+  function dfs(x: number, y: number): void {
+    if (visited[startPosition.Y][startPosition.X] === true && x === startPosition.X && y === startPosition.Y) {
+      console.log("Found it?");
+    }
+    if (x <= 0 || x >= pipeMap[0].length || y <= 0 || y >= pipeMap.length || visited[y][x]) {
+      return;
+    }
+
+    visited[y][x] = true;
+    console.log(`Visiting position: (${x}, ${y})`);
+
+    const currentPipe = pipeMap[y][x];
+
+    for (const direction of currentPipe.directions) {
+      const newPipePos = pipeMap[y + direction.Y][x + direction.X];
+      if (canMove(newPipePos, currentPipe, maxX, maxY, groundList, direction)) {
+        const newX = x + direction.X;
+        const newY = y + direction.Y;
+        dfs(newX, newY);
+      }
+    }
   }
-  if (current === "L") {
-    if (next === "-") return true;
-    if (next === "7") return true;
-    if (next === "J") return true;
+
+  dfs(startPosition.X, startPosition.Y);
+  return 0; // Update this return value based on your requirements
+}
+
+const north: Point = { X: 0, Y: -1 };
+const east: Point = { X: 1, Y: 0 };
+const south: Point = { X: 0, Y: 1 };
+const west: Point = { X: -1, Y: 0 };
+
+const PIPES: Map<string, Point[]> = new Map<string, Point[]>([
+  ["|", [north, south]],
+  ["-", [east, west]],
+  ["L", [north, east]],
+  ["J", [north, west]],
+  ["7", [south, west]],
+  ["F", [east, south]]
+]);
+const DELTAS: Point[] = [
+  { X: -1, Y: 0 },
+  { X: 1, Y: 0 },
+  { X: 0, Y: -1 },
+  { X: 0, Y: 1 }
+];
+
+function getDirections(pipe: string): Point[] {
+  const directions: Point[] = [];
+  switch (pipe) {
+    case "|":
+      directions.push(north, south);
+      break;
+    case "-":
+      directions.push(east, west);
+      break;
+    case "L":
+      directions.push(north, east);
+      break;
+    case "J":
+      directions.push(north, west);
+      break;
+    case "7":
+      directions.push(south, west);
+      break;
+    case "F":
+      directions.push(east, south);
+      break;
+    case ".":
+      break;
+    case "S":
+      break;
+    default:
+      throw new Error("Unknown Symbol");
   }
-  if (current === "F") {
-    if (next === "-") return true;
-    if (next === "7") return true;
-    if (next === "J") return true;
+  return directions;
+}
+
+export function SolvePartTwo(): number {
+  return 0;
+}
+
+function deepCopyArray(originalArray: PipePosition[][]): PipePosition[][] {
+  return originalArray.map((row) => row.slice());
+}
+function canMove(
+  direction: PipePosition,
+  currentPipe: PipePosition,
+  maxX: number,
+  maxY: number,
+  groundList: Point[],
+  newDirection: Point
+): boolean {
+  const isGround = groundList.find((g) => g.X === direction.currentCoordinate.X && g.Y === direction.currentCoordinate.Y);
+  if (isGround !== undefined) return false;
+  if (
+    0 <= direction.currentCoordinate.X &&
+    direction.currentCoordinate.X < maxX &&
+    0 <= direction.currentCoordinate.Y &&
+    direction.currentCoordinate.Y < maxY
+  ) {
+    console.log("Are " + currentPipe.pipe + " and " + direction.pipe + " connected");
+    if (arePipesConnecting(currentPipe.pipe, newDirection)) {
+      console.log("True");
+      return true;
+    } else {
+      console.log("False");
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+function arePipesConnecting(pipe1: string, pipe2: Point): boolean {
+  const directions1 = PIPES.get(pipe1);
+
+  if (!directions1) {
+    throw new Error("Invalid pipe type");
+  }
+  for (const dir1 of directions1) {
+    if (pipe2.X - dir1.X === 0 && pipe2.Y - dir1.Y === 0) {
+      return true;
+    }
   }
 
   return false;
