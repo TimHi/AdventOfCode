395e8aa6c440d04f0fdc33f46bf686835168a31e
TimHi <timhiller@live.de>
2023-12-14 03:19:27 +0100

    Part 01 bit cleaner, Part 2 needs rework

--- Typescript/2023/src/days/day13/day13.ts
+++ Typescript/2023/src/days/day13/day13.ts
@@ -25,8 +25,8 @@ export function SolvePartOne(): number {
   let horizontalRowSum = 0;
   let verticalRowSum = 0;
   fields.forEach((field, i) => {
-    const horizontalRows = testHorizontalReflection(field, 0, false);
-    const verticalColumns = testVerticalReflection(field, 0, false);
+    const horizontalRows = testHorizontalReflection(field, 0);
+    const verticalColumns = testVerticalReflection(field, 0);
     if (horizontalRows === undefined && verticalColumns === undefined) throw new Error("No reflection found for field: " + i);
     if (horizontalRows !== undefined && horizontalRows.row !== undefined) {
       oldReflectionMap.set(i, horizontalRows.row);
@@ -43,41 +43,12 @@ export function SolvePartOne(): number {
 interface SmudgeResult {
   row: number | undefined;
   wasHorizontal: boolean;
-  smudgeLines: string[];
 }
 
 export function SolvePartTwo(): number {
   const fields = parseFields();
-  let horizontalRowSum = 0;
-  let verticalRowSum = 0;
 
-  fields.forEach((field, i) => {
-    const horizontalSmudgeIndex = testHorizontalReflection(field, 1, true);
-    const verticalSmudgeIndex = testVerticalReflection(field, 1, true);
-    if (horizontalSmudgeIndex === undefined && verticalSmudgeIndex === undefined) throw new Error("No smudge found for field: " + i);
-
-    if (horizontalSmudgeIndex !== undefined && horizontalSmudgeIndex.row !== undefined) {
-      const result = correctSmudgeAndTest(field, horizontalSmudgeIndex, i);
-      if (result !== undefined && result.row !== undefined) {
-        if (result.wasHorizontal) {
-          horizontalRowSum += result.row;
-        } else {
-          verticalRowSum += result.row;
-        }
-      }
-    }
-    if (verticalSmudgeIndex !== undefined && verticalSmudgeIndex.row !== undefined) {
-      const result = correctSmudgeAndTest(field, verticalSmudgeIndex, i);
-      if (result !== undefined && result.row !== undefined) {
-        if (result.wasHorizontal) {
-          horizontalRowSum += result.row;
-        } else {
-          verticalRowSum += result.row;
-        }
-      }
-    }
-  });
-  return verticalRowSum + horizontalRowSum * 100;
+  return 0;
 }
 
 function isHammingDistanceMatch(one: string, two: string, match: number): boolean {
@@ -85,43 +56,7 @@ function isHammingDistanceMatch(one: string, two: string, match: number): boolea
   return hammingDistance(one, two) === match;
 }
 
-function correctSmudgeAndTest(field: string[], smudgeResult: SmudgeResult, fieldIndex: number): SmudgeResult {
-  let correctedField: string[] = [...field];
-  if (smudgeResult.wasHorizontal) {
-    if (smudgeResult.smudgeLines.length !== 2) throw new Error("Need 2 lines to fix smudge");
-    const smudgeColIndex = getSmudgeIndex(smudgeResult.smudgeLines[0], smudgeResult.smudgeLines[1]);
-    const correctedRow = exchangeCharAtIndex(smudgeResult.smudgeLines[0], smudgeColIndex);
-    correctedField[smudgeResult.row! + 1] = correctedRow;
-  } else {
-    const transposedField = transposeArray(correctedField.map((l) => l.split(""))).map((l) => l.join(""));
-    if (smudgeResult.smudgeLines.length !== 2) throw new Error("Need 2 lines to fix smudge");
-    const smudgeColIndex = getSmudgeIndex(smudgeResult.smudgeLines[0], smudgeResult.smudgeLines[1]);
-    const correctedRow = exchangeCharAtIndex(smudgeResult.smudgeLines[0], smudgeColIndex);
-    transposedField[smudgeResult.row!] = correctedRow;
-    correctedField = transposeArray(transposedField.map((l) => l.split(""))).map((l) => l.join(""));
-  }
-
-  const horizontalRows = testHorizontalReflection(correctedField, 0, false, true, fieldIndex);
-  const verticalColumns = testVerticalReflection(correctedField, 0, false, true, fieldIndex);
-  if (horizontalRows === undefined && verticalColumns === undefined) throw new Error("No reflection found for field: " + fieldIndex);
-  if (horizontalRows !== undefined && horizontalRows.row !== undefined) {
-    return horizontalRows;
-  }
-  if (verticalColumns !== undefined && verticalColumns.row !== undefined) {
-    return verticalColumns;
-  }
-
-  throw new Error("Did not find new reflection");
-}
-
-function testHorizontalReflection(
-  field: string[],
-  match: number,
-  isFindingSmudge: boolean,
-  checkForNew: boolean = false,
-  fieldIndex: number = 0
-): SmudgeResult {
-  let smudgeLines: string[] = [];
+function testHorizontalReflection(field: string[], match: number): SmudgeResult {
   let index = -1;
   for (let y = 0; y < field.length; y++) {
     index = -1;
@@ -131,24 +66,9 @@ function testHorizontalReflection(
         const upperRow = field[y - checkCount];
         const lowerRow = field[y + checkCount + 1];
         if (isHammingDistanceMatch(upperRow, lowerRow, match)) {
-          smudgeLines.push(upperRow, lowerRow);
-          if (isFindingSmudge) {
-            return { row: y, wasHorizontal: true, smudgeLines: smudgeLines }; //Holy shit I hate this
-          }
-          if (checkForNew) {
-            //Hacky way to avoid detecting the same old reflection before the new one and returning prematurely
-            const oldIndex = oldReflectionMap.get(fieldIndex);
-            if (oldIndex === undefined) throw new Error("Part One did not create reflection map correctly");
-            if (oldIndex !== y) {
-              smudgeLines.push(upperRow, lowerRow);
-              index = y;
-            }
-          } else {
-            index = y;
-          }
+          index = y;
         } else {
           index = -1;
-          smudgeLines = [];
           break;
         }
       }
@@ -156,30 +76,20 @@ function testHorizontalReflection(
     if (index !== -1) break;
   }
   // Add one to index because we want the row not the index
-  return { row: index !== -1 ? index + 1 : undefined, wasHorizontal: true, smudgeLines: smudgeLines };
+  return { row: index !== -1 ? index + 1 : undefined, wasHorizontal: true };
 }
 
-function testVerticalReflection(
-  field: string[],
-  match: number,
-  isFindingSmudge: boolean,
-  checkForNew: boolean = false,
-  fieldIndex: number = 0
-): SmudgeResult {
+function testVerticalReflection(field: string[], match: number): SmudgeResult {
   const fieldCopy = [...field];
   const transposedField = transposeArray(fieldCopy.map((l) => l.split(""))).map((l) => l.join(""));
-  const result = testHorizontalReflection(transposedField, match, isFindingSmudge, checkForNew, fieldIndex);
-  return { row: result.row, wasHorizontal: false, smudgeLines: result.smudgeLines };
+  const result = testHorizontalReflection(transposedField, match);
+  return { row: result.row, wasHorizontal: false };
 }
 
 function transposeArray(matrix: string[][]): string[][] {
-  console.log("Transpose Field");
   const rows = matrix.length;
   const cols = matrix[0].length;
-
-  // Create a new array with swapped rows and columns
   const transposedMatrix: string[][] = Array.from({ length: cols }, () => Array(rows).fill(""));
-
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       transposedMatrix[j][i] = matrix[i][j];
@@ -187,21 +97,4 @@ function transposeArray(matrix: string[][]): string[][] {
   }
 
   return transposedMatrix;
-}
-
-function exchangeCharAtIndex(input: string, index: number): string {
-  if (input[index] === ".") return input.substring(0, index) + "#" + input.substring(index + 1);
-  else return input.substring(0, index) + "." + input.substring(index + 1);
-}
-function getSmudgeIndex(one: string, two: string): number {
-  const maxLength = Math.max(one.length, two.length);
-  for (let i = 0; i < maxLength; i++) {
-    const char1 = one.charCodeAt(i) || 0;
-    const char2 = two.charCodeAt(i) || 0;
-
-    if ((char1 ^ char2) !== 0) {
-      return i;
-    }
-  }
-  throw new Error("Smudge expected to be found but XOR returns no match");
 }
