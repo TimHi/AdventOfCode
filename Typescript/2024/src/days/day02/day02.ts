2cacee793678b784058654c72e9b7f3e025350a7
TimHi <timhiller@live.de>
2024-12-02 15:20:44 +0100

    Part 02 yikes

--- Typescript/2024/src/days/day02/day02.ts
+++ Typescript/2024/src/days/day02/day02.ts
@@ -35,14 +35,51 @@ function isLevelvalid(level: number[]): boolean {
   return true;
 }
 
+function isLevelvalidWithDampener(level: number[]): boolean {
+  const growthDirection = determineGrowthDirection(level[0], level[1]);
+  let unsafeValueIndex = -1;
+  if (growthDirection === Direction.ERR) unsafeValueIndex = 0;
+
+  //No Error yet, continue checking
+  if (unsafeValueIndex === -1) {
+    for (let i = 0; i < level.length - 1; i++) {
+      if (determineGrowthDirection(level[i], level[i + 1]) !== growthDirection) {
+        unsafeValueIndex = i;
+        break;
+      }
+      if (!isIncreasingGradually(level[i], level[i + 1])) {
+        unsafeValueIndex = i;
+        break;
+      }
+    }
+  }
+
+  //Test wether a unsafe location was found, remove the unsafe location and throw that shit in Part 01
+  if (unsafeValueIndex !== -1) {
+    const aLevel = [...level];
+    const cLevel = [...level];
+    const dLevel = [...level];
+    if (unsafeValueIndex > 0) {
+      cLevel.splice(unsafeValueIndex - 1, 1);
+    }
+    dLevel.splice(unsafeValueIndex + 1, 1);
+    aLevel.splice(unsafeValueIndex, 1);
+
+    return isLevelvalid(aLevel) || isLevelvalid(cLevel) || isLevelvalid(dLevel);
+  } else {
+    return true;
+  }
+}
+
+function parseInput(raw: string): number[][] {
+  return raw.split("\n").map((line) => line.split(" ").map((sLine) => Number(sLine)));
+}
+
 export function SolvePartOne(): number {
   const fileName = isSample ? "/src/days/day02/sample.txt" : "/src/days/day02/full.txt";
   let safeRows = 0;
 
-  const levels = fs
-    .readFileSync(process.cwd() + fileName, "utf8")
-    .split("\n")
-    .map((line) => line.split(" ").map((sLine) => Number(sLine)));
+  const levels = parseInput(fs.readFileSync(process.cwd() + fileName, "utf8"));
 
   levels.forEach((level) => {
     if (isLevelvalid(level)) safeRows++;
@@ -52,6 +89,13 @@ export function SolvePartOne(): number {
 }
 
 export function SolvePartTwo(): number {
-  console.log("TBD");
-  return 0;
+  let safeRows = 0;
+  const fileName = isSample ? "/src/days/day02/sample.txt" : "/src/days/day02/full.txt";
+  const levels = parseInput(fs.readFileSync(process.cwd() + fileName, "utf8"));
+
+  levels.forEach((level) => {
+    if (isLevelvalidWithDampener(level)) safeRows++;
+  });
+
+  return safeRows;
 }
