e5160f97f7a25f560ba12487b5efffc335011b9b
TimHi <timhiller@live.de>
2024-12-08 15:12:59 +0100

    d08 wip

--- Typescript/2024/src/days/day08/day08.ts
+++ Typescript/2024/src/days/day08/day08.ts
@@ -1,10 +1,43 @@
+import { Point } from "aoc-util";
+import { HashMap } from "data-structure-typed";
 import * as fs from "fs";
 
 const isSample = true;
+function getAntennaLocations(map: string[][]): HashMap<string, Point[]> {
+  const antennaLocations: HashMap<string, Point[]> = new HashMap();
+
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[0].length; x++) {
+      const antenna = map[y][x];
+
+      if (antenna !== ".") {
+        const location: Point = { X: x, Y: y };
+        if (antennaLocations.has(antenna)) {
+          const locations = antennaLocations.get(antenna);
+          if (locations === undefined) throw new Error("Antenna locations in map should not be undefined");
+          locations.push(location);
+          antennaLocations.set(antenna, locations);
+        } else {
+          antennaLocations.set(antenna, [location]);
+        }
+      }
+    }
+  }
+  return antennaLocations;
+}
+
+function getAntinodeLocations(maxX: number, maxY: number, AntennaLocations: HashMap<string, Point[]>) {
+  const antinodeLocations: HashMap<string, Point[]> = new HashMap();
+}
 
 export function SolvePartOne(): number {
   const fileName = isSample ? "/src/days/day08/sample.txt" : "/src/days/day08/full.txt";
-
+  const lines = fs
+    .readFileSync(process.cwd() + fileName, "utf8")
+    .split("\n")
+    .map((l) => l.split(""));
+  const AntennaLocations: HashMap<string, Point[]> = getAntennaLocations(lines);
+  const AntinodeLocations: HashMap<string, Point[]> = getAntinodeLocations(lines[0].length, lines.length, AntennaLocations);
   return 0;
 }
 
--- Typescript/2024/src/main.ts
+++ Typescript/2024/src/main.ts
@@ -4,7 +4,7 @@ type Solver = {
 };
 
 const solvers: Solver[] = [];
-const day: number = 7;
+const day: number = 8;
 
 async function loadSolvers() {
   for (let i = 1; i <= day; i++) {
