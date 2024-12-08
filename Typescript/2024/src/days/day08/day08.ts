4679458c8ff94dad65596b8997a890e8f06462c1
TimHi <TimHiller@live.de>
2024-12-08 12:36:19 +0100

    part 8 seceleton

---
+++ Typescript/2024/src/days/day08/day08.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+
+const isSample = true;
+
+export function SolvePartOne(): number {
+  const fileName = isSample ? "/src/days/day08/sample.txt" : "/src/days/day08/full.txt";
+
+  return 0;
+}
+
+export function SolvePartTwo(): number {
+  return 0;
+}
