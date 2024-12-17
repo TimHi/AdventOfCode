2b618f2355f719415ee8db1166a43b8d76509926
TimHi <TimHiller@live.de>
2024-12-17 17:23:12 +0100

    d17

--- Typescript/2024/src/days/day17/day17.ts
+++ Typescript/2024/src/days/day17/day17.ts
@@ -1,7 +1,7 @@
 import { getAllNumbersInString } from "aoc-util";
 import * as fs from "fs";
 
-const isSample = true;
+const isSample = false;
 
 function getOpCode(n: number): Opcode {
   if (n === 0) return Opcode.adv;
@@ -62,11 +62,13 @@ interface Computer {
   B: number;
   C: number;
   program: Operation[];
+  inst: number;
+  out: number[];
 }
 
 export function SolvePartOne(): number {
   const fileName = isSample ? "/src/days/day17/sample.txt" : "/src/days/day17/full.txt";
-  const pc: Computer = { A: 0, B: 0, C: 0, program: [] };
+  const pc: Computer = { A: 0, B: 0, C: 0, program: [], inst: 0, out: [] };
   const lines = fs.readFileSync(process.cwd() + fileName, "utf8").split("\n");
 
   pc.A = getAllNumbersInString(lines[0])[0];
@@ -78,58 +80,143 @@ export function SolvePartOne(): number {
     .map((l) => Number(l));
 
   const operations: Operation[] = [];
-  for (let i = 0; i < raw.length - 1; i++) {
+  for (let i = 0; i < raw.length - 1; i += 2) {
     operations.push({ code: raw[i], comboOperand: raw[i + 1] });
   }
   pc.program = operations;
+  const finishedPc = runProgram(pc);
+  console.log(pc);
+  console.log(finishedPc.out.map((n) => String(n)).join(","));
   return 0;
 }
 
 export function SolvePartTwo(): number {
   const fileName = isSample ? "/src/days/day17/sample.txt" : "/src/days/day17/full.txt";
-  const pc: Computer = { A: 0, B: 0, C: 0, program: [] };
+  const pc: Computer = { A: 0, B: 0, C: 0, program: [], inst: 0, out: [] };
   const lines = fs.readFileSync(process.cwd() + fileName, "utf8").split("\n");
 
   pc.A = getAllNumbersInString(lines[0])[0];
   pc.B = getAllNumbersInString(lines[1])[0];
   pc.C = getAllNumbersInString(lines[2])[0];
-
   const raw = lines[4]
     .split(" ")[1]
     .split(",")
     .map((l) => Number(l));
 
-  const output = runProgram(pc);
-  return Number(output.map((n) => String(n)).join());
-}
-
-function getComboOperand(op: Opcode, pc: Computer): number {
-  if (op === Opcode.adv) {
+  const operations: Operation[] = [];
+  for (let i = 0; i < raw.length - 1; i += 2) {
+    operations.push({ code: raw[i], comboOperand: raw[i + 1] });
   }
+  pc.program = operations;
+
+  const output = runProgram(pc).out;
+  console.log(pc);
+  console.log(finishedPc.out.map((n) => String(n)).join(","));
   return 0;
 }
 
-function runProgram(pc: Computer): number[] {
-  for (let instPointer = 0; instPointer < pc.program.length; instPointer++) {
-    const op = pc.program[instPointer];
+function getComboOperandValue(op: Operation, pc: Computer): number {
+  if (op.comboOperand === 0) return 0;
+  if (op.comboOperand === 1) return 1;
+  if (op.comboOperand === 2) return 2;
+  if (op.comboOperand === 3) return 3;
+  if (op.comboOperand === 4) return pc.A;
+  if (op.comboOperand === 5) return pc.B;
+  if (op.comboOperand === 6) return pc.C;
+  if (op.comboOperand === 7) return 7;
+  throw new Error("Unknown combo operand");
+}
+
+function adv(op: Operation, pc: Computer): Computer {
+  const numerator = pc.A;
+  const denumerator = getComboOperandValue(op, pc);
+  pc.A = Math.floor(numerator / Math.pow(2, denumerator));
+  return pc;
+}
+
+function bxl(op: Operation, pc: Computer): Computer {
+  const operand = op.comboOperand;
+  // ^ bitwise XOR
+  pc.B = pc.B ^ operand;
+  return pc;
+}
+
+function bst(op: Operation, pc: Computer): Computer {
+  pc.B = getComboOperandValue(op, pc) % 8;
+  return pc;
+}
+
+function jnz(op: Operation, pc: Computer): Computer {
+  const operand = op.comboOperand;
+  if (pc.A !== 0) {
+    pc.inst = operand;
+  } else {
+    pc.inst += 1;
+  }
+  return pc;
+}
+
+function bxc(pc: Computer): Computer {
+  pc.B = pc.B ^ pc.C;
+  return pc;
+}
+
+function out(op: Operation, pc: Computer): Computer {
+  const operand = getComboOperandValue(op, pc);
+  pc.out.push(operand % 8);
+  return pc;
+}
+
+function bdv(op: Operation, pc: Computer): Computer {
+  const numerator = pc.A;
+  const denumerator = getComboOperandValue(op, pc);
+  pc.B = Math.floor(numerator / Math.pow(2, denumerator));
+  return pc;
+}
+
+function cdv(op: Operation, pc: Computer): Computer {
+  const numerator = pc.A;
+  const denumerator = getComboOperandValue(op, pc);
+  pc.C = Math.floor(numerator / Math.pow(2, denumerator));
+  return pc;
+}
+
+function runProgram(pc: Computer): Computer {
+  while (pc.inst < pc.program.length) {
+    const op = pc.program[pc.inst];
     switch (op.code) {
       case Opcode.adv:
+        pc = adv(op, pc);
+        pc.inst += 1;
         break;
       case Opcode.bxl:
+        pc = bxl(op, pc);
+        pc.inst += 1;
         break;
       case Opcode.bst:
+        pc = bst(op, pc);
+        pc.inst += 1;
         break;
       case Opcode.jnz:
+        pc = jnz(op, pc);
         break;
       case Opcode.bxc:
+        pc = bxc(pc);
+        pc.inst += 1;
         break;
       case Opcode.out:
+        pc = out(op, pc);
+        pc.inst += 1;
         break;
       case Opcode.bdv:
+        bdv(op, pc);
+        pc.inst += 1;
         break;
       case Opcode.cdv:
+        cdv(op, pc);
+        pc.inst += 1;
         break;
     }
   }
-  return [];
+  return pc;
 }
