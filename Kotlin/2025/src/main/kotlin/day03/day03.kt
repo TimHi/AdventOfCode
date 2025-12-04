3462d27b760149fa68fd06712bffd7b202c46abf
TimHi <TimHiller@live.de>
2025-12-04 15:26:19 +0100

    day 03 part 2

--- Kotlin/2025/src/main/kotlin/day03/day03.kt
+++ Kotlin/2025/src/main/kotlin/day03/day03.kt
@@ -1,6 +1,5 @@
 package day03
 
-
 fun readFile(path: String): List<String> {
     val ranges = mutableListOf<String>();
     {}.javaClass.getResourceAsStream(path)?.bufferedReader()?.forEachLine {
@@ -23,14 +22,11 @@ fun main() {
 
 }
 
-fun getIndexOfHighestNumberInIntArrayWhenIJustNeedTheFirstOccurence(a: List<Int>): Int {
+fun getIndexOfHighestNumberInIntArrayWhenIJustNeedTheFirstOccurrence(a: List<Int>): Int {
     val highestNumber = a.maxOrNull()
     return a.indexOfFirst { it == highestNumber }
 }
 
-//23 42 34 23 42 34 27 8
-//43 58 54 34 24 52 35 43 45 16 34 32 24 26 44 52 23 33 43 23 14 23 33
-//44 34 44 44 34 34 54 45 65 43 42 54 47 43 43 54 42 33 44 32 24 45 45 43 44 69 63
 fun partOne(sampleFlag: Boolean) {
     var sum: Long = 0
     val path = if (sampleFlag) "/day03/sample.txt" else "/day03/input.txt";
@@ -39,10 +35,11 @@ fun partOne(sampleFlag: Boolean) {
         val iList1 = it.split("").filter { l -> l.isNotEmpty() && l.isNotBlank() }
         val iList = iList1.map { n -> n.toInt() }
         val noEnd = iList.subList(0, iList.size - 1)
-        val highestNumber = getIndexOfHighestNumberInIntArrayWhenIJustNeedTheFirstOccurence(noEnd)
+        val highestNumber = getIndexOfHighestNumberInIntArrayWhenIJustNeedTheFirstOccurrence(noEnd)
         val restOfTheBattery = iList.subList(highestNumber + 1, iList.size)
-        val secondHighestNumber = getIndexOfHighestNumberInIntArrayWhenIJustNeedTheFirstOccurence(restOfTheBattery)
-        val putTogether = listOf(noEnd[highestNumber].toString(), restOfTheBattery[secondHighestNumber].toString()).joinToString("")
+        val secondHighestNumber = getIndexOfHighestNumberInIntArrayWhenIJustNeedTheFirstOccurrence(restOfTheBattery)
+        val putTogether =
+            listOf(noEnd[highestNumber].toString(), restOfTheBattery[secondHighestNumber].toString()).joinToString("")
         sum += putTogether.toInt()
     }
     println("Output joltage $sum")
@@ -51,7 +48,38 @@ fun partOne(sampleFlag: Boolean) {
 fun partTwo(sampleFlag: Boolean) {
     var sum: Long = 0
     val path = if (sampleFlag) "/day03/sample.txt" else "/day03/input.txt";
+    val bat = readFile(path)
+    bat.forEach {
+        val iList1 = it.split("").filter { l -> l.isNotEmpty() && l.isNotBlank() }.map { n -> n.toInt() }
+        var checkList = iList1
+        val batteryRange = 12
+        val batteryDigits = mutableListOf<Int>()
+
+        for (r in batteryRange downTo 1) {
+            val highestNumberIndex = getIndexOfHighestNumberInRange(checkList, r)
+            batteryDigits.add(checkList[highestNumberIndex])
+
+            //We have the highest number, now build from sublist
+            checkList = checkList.subList(highestNumberIndex + 1, checkList.size)
+        }
+
+        val builtNum = batteryDigits.joinToString("")
+        sum += builtNum.toLong()
+    }
 
     println("Sum: $sum")
 }
 
+fun getIndexOfHighestNumberInRange(list: List<Int>, range: Int): Int {
+    var checkList = list
+    var highestNumberIndex = 0
+    while (true) {
+        highestNumberIndex = getIndexOfHighestNumberInIntArrayWhenIJustNeedTheFirstOccurrence(checkList)
+        if ((highestNumberIndex + range) >= list.size + 1) {
+            checkList = list.subList(0, highestNumberIndex)
+        } else {
+            break
+        }
+    }
+    return highestNumberIndex
+}
